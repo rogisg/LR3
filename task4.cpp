aa246415f78f085b13da8e3ae6818125e561c69c
rogisg <gogalebedev91@gmail.com>
2024-10-12 15:09:48 +0300

    task4.cpp

---
+++ task4.cpp
@@ -0,0 +1,57 @@
+//задача 4
+
+#include <iostream>
+#include <ctime>
+
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int n;
+    cin>>n;
+
+
+    double arr[n];
+    
+
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 100 - 50;
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        cout << "arr[" << i << "] = " << arr[i] << endl;
+    }
+    
+    
+     for (int i = 0; i < n; i++) {
+        if (arr[i] < 0) {
+            for (int j = i; j < n - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+            n--;
+            break;
+        }
+    }
+    
+    
+    
+    for (int i = n - 1; i >= 0; i--) {
+        if (int(arr[i]) % 2 == 0) {
+            for (int j = i; j < n - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+            n--;
+            break;
+        }
+    }
+    
+    cout<<"massiv posle udaleniya pervogo otritsatelnogo i poslednego chetnogo el-ov:"<<endl;
+    for (int i = 0; i < n; i++) {
+        cout <<"arr[" << i << "] = " << arr[i] << endl;
+    }
+        
+    return 0;
+}
