3d03d0f53168ab46be8f8e572264a086eeff94ae
rogisg <gogalebedev91@gmail.com>
2024-10-12 14:04:26 +0300

    task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,27 @@
+//задача 1
+
+#include <iostream>
+
+using namespace std;
+int main() {
+    int n, a;
+    cin >> n >> a;
+    
+    
+    double arr[n];
+    for (int i = 0; i < n + 1; i++) {
+        arr[i] = rand() % 100; 
+    }
+    for (int i = 0; i < n; i++){
+        cout << "arr" << i << "=" << arr[i] << endl;
+    }
+    int f_el = arr[0];
+    for (int i = 0; i < n; i++){
+        arr[i] = (((arr[i]*2)-a)/f_el);
+    }    
+    
+    for (int i = 0; i < n; i++){
+        cout << "arr_new" << i << "=" << arr[i] << endl;
+    }
+    return 0;
+}
